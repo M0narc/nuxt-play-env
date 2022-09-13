d151a2d146a2ec52d5f3bf219dba494d4678fd37
M0narc <francomatiaslujan23@gmail.com>
2022-09-13 16:11:34 -0300

    new Header component, plugin folder with bootstrap, nuxt.config implemented plugin and new Header component implemented in layouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,132 +1,17 @@
 <template>
   <selection id="home" class="container">
-  <ul>
-    <li><nuxt-link to="/users">Users</nuxt-link></li>
-    <li><nuxt-link to="/products">product</nuxt-link></li>
-  </ul>
+    <ul>
+      <li>
+        <nuxt-link to="/users">Users</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/products">product</nuxt-link>
+      </li>
+    </ul>
   </selection>
 </template>
 
 
 <style>
-html {
-  font-family: 'Jost', sans-serif;
-}
 
-body {
-  margin: 0;
-}
-
-header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #4fc08d;
-  color: white;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-
-#home {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-#home a {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #4fc08d;
-  margin: 0 0 1rem 0;
-}
-
-#home h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #4fc08d;
-  margin: 0 0 1rem 0;
-}
-
-#home p {
-  font-size: 1.25rem;
-  font-weight: bold;
-  border: 1px solid #4fc08d;
-  background-color: #4fc08d;
-  color: white;
-  padding: 0.5rem;
-  border-radius: 25px;
-}
-
-#home input {
-  font: inherit;
-  border: 1px solid #ccc;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-  border-radius: 20px;
-  margin: 0.2rem;
-}
-
-#home input:focus {
-  outline: none;
-  border-color: #1b995e;
-  background-color: #d7fdeb;
-}
-
-#home button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-  border-radius: 20px;
-  margin: 0 1rem;
-}
-
-#home button:hover,
-#home button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
-}
-
-.container
-{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
 </style>
